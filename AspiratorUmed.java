e4fc434906ee9b510fd1539fb33be51e45f158c1
MyIdolFane <stefancaloian9@gmail.com>
2024-11-19 08:20:29 +0200

    Update AspiratorUmed.java

--- AspiratorUmed.java
+++ AspiratorUmed.java
@@ -1,34 +1,30 @@
 package Pachet;
 
-public class AspiratorUscat extends Aparat {
-	private int putereAspirare; 
-    private double capacitateRezervor; 
-    private boolean areFiltruHEPA;
-    
-    public AspiratorUscat() {
-    	  super();
-          this.putereAspirare = 0;
-          this.capacitateRezervor = 0.0;
-          this.areFiltruHEPA = false;	
-    }
-    public AspiratorUscat(String brand,String model, double pret, int garantie, String categorie, int putereAspirare, double capacitateRezervor, boolean areFiltruHEPA) {
-    	super(brand, model, pret, garantie, categorie);
-    	this.putereAspirare = putereAspirare;
-    	this.capacitateRezervor = capacitateRezervor;
-    	this.areFiltruHEPA = areFiltruHEPA;
-    }
-    public AspiratorUscat(AspiratorUscat other) {
-    	super(other);
-        this.putereAspirare = other.putereAspirare;
-        this.capacitateRezervor = other.capacitateRezervor;
-        this.areFiltruHEPA = other.areFiltruHEPA;
-    } 
-    public String toString() {
-        return "Aspirator{" +
-                super.toString() +
-                ", putereAspirare=" + putereAspirare +
-                ", capacitateRezervor=" + capacitateRezervor +
-                ", areFiltruHEPA=" + areFiltruHEPA +
-                '}';
-}
+public class AspiratorUmed extends AspiratorUscat{
+	 private boolean suportaLichide;
+	 private boolean areFunctieUscare;
+	 
+	 public AspiratorUmed() {
+		 super();
+	     this.suportaLichide = false;
+	     this.areFunctieUscare = false;
+	 }
+	 public AspiratorUmed(String brand,String model, double pret, int garantie, String categorie, int putereAspirare, double capacitateRezervor, boolean areFiltruHEPA, boolean suportaLichide, boolean areFunctieUscare) {
+		 super(brand, model, pret, garantie, categorie, putereAspirare, capacitateRezervor, areFiltruHEPA);
+		 this.suportaLichide = suportaLichide;
+	     this.areFunctieUscare = areFunctieUscare;
+	}
+	 public AspiratorUmed(AspiratorUmed other) {
+	        super(other);
+	        this.suportaLichide = other.suportaLichide;
+	        this.areFunctieUscare = other.areFunctieUscare;
+	    }
+	 public String toString() {
+	        return "AspiratorUmed{" +
+	                super.toString() +
+	                ", suportaLichide=" + suportaLichide +
+	                ", areFunctieUscare=" + areFunctieUscare +
+	                '}';
+	    }
+
 }
